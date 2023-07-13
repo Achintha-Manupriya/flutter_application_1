66e10471e6008c33570764fcae961753dbc0479c
Achintha Manupriya <achinthamanupriya7@gmail.com>
2023-07-13 18:42:28 +0530

    First commit

---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/main.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //return const HomePage();
+
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("Achintha"),
+          elevation: 4,
+          backgroundColor: Color.fromARGB(255, 49, 132, 249)),
+      body: const Center(
+        child: Text(
+          "Welcome to my first application",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold
+
+              // Set the desired font size
+              ),
+        ),
+      ),
+    );
+  }
+}
